dbacf8f2d78bda0da3332e75a83e183f1f0a1c33
heshame <atsh@gmail.com>
2024-09-19 20:47:27 +0300

    test

--- config/cors.php
+++ config/cors.php
@@ -12,6 +12,7 @@ return [
     | in web browsers. You are free to adjust these settings as needed.
     |
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    /////////////////////////////////////////////////////////// test
     |
     */
 
